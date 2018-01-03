dba901c1e41669478d52cc72f7f7b606b87d1310
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-01-03 09:32:53 -0600

    Update install requires/requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup
 
 setup(
     name='folditdb',
+    version='0.0.1',
     packages=['folditdb'],
+    install_requires=[
+     'SQLAlchemy==1.2.0',
+     'PyMySQL==0.8.0',
+    ],
     entry_points={
         'console_scripts': [
             'folditdb=folditdb:main',
