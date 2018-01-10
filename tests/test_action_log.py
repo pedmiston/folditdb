d2775452255315f9c1662433a51afc5aee26b622
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-01-10 13:03:24 -0600

    Add new test cases

--- tests/test_action_log.py
+++ tests/test_action_log.py
@@ -4,6 +4,7 @@ from folditdb.load import load_single_irdata_file
 from folditdb.irdata import IRData, PDL, ActionLog
 from folditdb.tables import Action
 
+
 def test_parse_action_string():
     action_str = "|ActionBandAddAtomAtom=6 |ActionBandDelete=5 |ActionDeleteCut=8"
     actions = ActionLog.from_action_string(action_str)
@@ -11,6 +12,12 @@ def test_parse_action_string():
     first_action = actions[0]
     assert first_action.action_name == 'ActionBandAddAtomAtom'
     assert first_action.action_n == 6
+
+def test_parse_empty_action_log():
+    irdata = IRData.from_file('tests/test_data/solution_without_action_log.json')
+    pdl = PDL.from_irdata(irdata)[0]
+    action_logs = ActionLog.from_pdl(pdl)
+    assert len(action_logs) == 0
 
 def test_parse_complex_action_string():
     action_str = "Pull_Mode|ACTIVATE=3 Selection_Interface|BringUpTweakWidget=5 Structure_Mode|ACTIVATE=3 |=8"
@@ -28,3 +35,11 @@ def test_actions_are_parsed_from_solution_with_two_players(tmp_log, session):
     first_action = player.actions[0]
     assert first_action.action_name == 'ActionBandAddAtomAtom'
     assert first_action.action_n == 6
+
+def test_duplicate_actions_per_player_are_created(tmp_log, session):
+    load_single_irdata_file('tests/test_data/multiple_pdls_same_player.json', session)
+    player = session.query(tables.Player).filter_by(name='retiredmichael').first()
+    dup_actions = session.query(tables.Action).filter_by(player_id=player.id, action_name='ActionStandaloneResetRecentBest').all()
+    assert len(dup_actions) == 2
+    assert dup_actions[0].action_n == 200
+    assert dup_actions[1].action_n == 519
