292318b67a2b55598f89745c071a5fe7600dca40
Pierce Edmiston <pierce.edmiston@gmail.com>
2018-01-10 20:02:46 -0600

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='folditdb',
-    version='0.0.3',
+    version='0.0.4',
     packages=['folditdb'],
     install_requires=[
      'SQLAlchemy==1.2.0',
